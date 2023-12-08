fa7249d352ead49893712bd6d728d47189a7eb28
takumi-333 <takumi_soccer0514@icloud.com>
2023-12-09 08:42:31 +0900

    OpenStreetMap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import 'leaflet/dist/leaflet.css';
 import Map from './Components/Map';
 import { useState } from 'react';
 let apikey = process.env.REACT_APP_API_KEY;
 
 function App() {
-    const [ gps, setGps ] = useState({lat: "35.6814568602531", lng: "139.76799772026422"});
+    const [ gps, setGps ] = useState([35.03937, 135.724737]);
+    // const [ gps, setGps ] = useState({lat: "35.6814568602531", lng: "139.76799772026422"});
     return (
     <div>
       
